eabc5cff35a64a5415656510894567188c40f35d
Yiqian9 <39084940+Yiqian9@users.noreply.github.com>
2021-10-01 21:05:09 +0800

    testjsindustry

--- story_content/user.js
+++ story_content/user.js
@@ -2,23 +2,29 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "5XznYBlgAqK":
+      case "5hPcD0tYKFN":
         Script1();
         break;
-      case "5W9FCPX1u6P":
+      case "6pr9gMlidmr":
         Script2();
         break;
-      case "5wwynJUvuJz":
+      case "6hB3cKbrPp2":
         Script3();
         break;
-      case "6JZIlBUrG09":
+      case "6DoBlM8a2hd":
         Script4();
         break;
-      case "6ZER0d4SgXa":
+      case "5lpVLpizPNZ":
         Script5();
         break;
-      case "6WWmXXUf0LC":
+      case "6EWhQwfZjc1":
         Script6();
+        break;
+      case "5klxaQxMIFT":
+        Script7();
+        break;
+      case "6REo7vuEFnl":
+        Script8();
         break;
   }
 }
@@ -37,7 +43,8 @@ function Script2()
   var player = GetPlayer();
  
 //PLACE YOUR WEB APP URL
-WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyNSu62mpo694H4IsFtjgLayeXNgR-rKJAMMqOdqVD7eTI_YKsXqenCgFDKsUTFX1YSMw/exec";
+WEB_APP_URL = "
+https://script.google.com/macros/s/AKfycbxN7njMDweVzPRISQ6MRIVIcKxC0PR53z4D_0oS_YDu2JJ9hYI-8E0ovN7YKvvYo7CLKg/exec";
  
 // STORE ARTICULATE STORYLINE VARIABLES
 // "Columnname_Google_Spreadsheet" : player.GetVar("Name_Storyline_Variable")
@@ -46,11 +53,35 @@ storyline =
 {
  "date" : new Date().toJSON().slice(0,10), //STORE DATE
  "industry" : player.GetVar("industry"),
- "answer" : "covering",
 }
 }
 
 function Script3()
+{
+  var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+head.appendChild(script)
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+ 
+//PLACE YOUR WEB APP URL
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxN7njMDweVzPRISQ6MRIVIcKxC0PR53z4D_0oS_YDu2JJ9hYI-8E0ovN7YKvvYo7CLKg/exec";
+ 
+// STORE ARTICULATE STORYLINE VARIABLES
+// "Columnname_Google_Spreadsheet" : player.GetVar("Name_Storyline_Variable")
+// ATTENTION: Use a comma if you use multiple Storyline variables
+storyline =
+{
+ "answer" : "covering",
+}
+}
+
+function Script5()
 {
   //DELAY SO JQUERY LIBRARY IS LOADED
 setTimeout(function (){
@@ -72,7 +103,7 @@ return false;
 }, 1000);
 }
 
-function Script4()
+function Script6()
 {
   var head = document.getElementsByTagName('head')[0];
 var script = document.createElement('script');
@@ -81,25 +112,24 @@ script.type = 'text/javascript';
 head.appendChild(script)
 }
 
-function Script5()
+function Script7()
 {
   var player = GetPlayer();
  
 //PLACE YOUR WEB APP URL
-WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyNSu62mpo694H4IsFtjgLayeXNgR-rKJAMMqOdqVD7eTI_YKsXqenCgFDKsUTFX1YSMw/exec";
+WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxN7njMDweVzPRISQ6MRIVIcKxC0PR53z4D_0oS_YDu2JJ9hYI-8E0ovN7YKvvYo7CLKg/exec";
  
 // STORE ARTICULATE STORYLINE VARIABLES
 // "Columnname_Google_Spreadsheet" : player.GetVar("Name_Storyline_Variable")
 // ATTENTION: Use a comma if you use multiple Storyline variables
 storyline =
 {
- "date" : new Date().toJSON().slice(0,10), //STORE DATE
- "industry" : player.GetVar("industry"),
+ 
  "answer" : "space",
 }
 }
 
-function Script6()
+function Script8()
 {
   //DELAY SO JQUERY LIBRARY IS LOADED
 setTimeout(function (){
